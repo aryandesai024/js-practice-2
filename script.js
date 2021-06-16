9e3ed2c7e891894e220b96026001606a817398cb
Aryan Desai <73381496+aryandesai024@users.noreply.github.com>
2021-06-16 13:06:52 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,70 +1,70 @@
+let increament = document.querySelector("#increment");
+let decreament = document.querySelector("#decrement");
+let reset = document.querySelector("#reset");
+let counterValue = document.querySelector("#counterValue");
 
-document.addEventListener('DOMContentLoaded', function () {
+let counter = 0;
 
-    //Get All Quotes
-const quotes = [
-    {
-        quote:
-            "Code Never Lies, Comments Sometimes Do.",
-        author: "Ron Jeffries"
-    },
-    {
-        quote:
-            "Programmers are constantly making things more complicated than they need to be because future. Program for Today.",
-        author: "David Heinemeier Hansson"
-    },
-    {
-        quote:
-            "Code is like humor. When you have to explain it, It's Bad.",
-        author: "Cory House"
-    },
-    {
-        quote: "The proper use of comments is to compensate for our failure to express ourself in code.",
-        author: "Robert C Martin"
-    },
-    {
-        quote:
-            "If You Can't deploy your services independently then they are not microservice. ",
-        author: "Daniel Bryant"
-    },
-    {
-        quote:
-            "Programming is key concept who want to understand and must have interested of ourself.",
-        author: " Code Express"
-    },
-    {
-        quote:
-            "Don't be pushed around by the fears in your mind. Be led by the dreams in your heart.",
-        author: " Roy T. Bennett"
-    },
-    {
-        quote:
-            "It’s only after you’ve stepped outside your comfort zone that you begin to change, grow, and transform.",
-        author: " Roy T. Bennett"
+increment.addEventListener('click', ()=>{
+    counter++;
+    counterValue.innerText = counter;
+    if(counterValue.innerText > 0) {
+        counterValue.style.color = '#3ddcef'; 
     }
-];
-
-    // GET ELEMENTS
-    const button = document.getElementById('btn');
-    const content = document.getElementById('quote');
-    const author = document.getElementById('author');
-
-    // add click event to the button
-    button.addEventListener('click', fetchQuote);
-
-    function fetchQuote() {
-        button.innerText = "Loading...";
-        button.disabled = true;
-
-        let random = Math.floor(Math.random() * quotes.length);
-
-
-        setTimeout(()=>{
-                document.querySelector('.quote').innerText = quotes[random].quote;
-                document.querySelector('#author').innerText = quotes[random].author;
-                button.disabled = false;
-                button.innerText = "Get New Quote"
-        },800);
+    else if(counterValue.innerText == 0) {
+        counterValue.style.color = '#000';
+    } else {
+        counterValue.style.color = 'red';
     }
+    counterValue.animate([
+        {
+            opacity: '0.5'
+        },
+        {
+            opacity: '1,0'
+        }
+    ], {duration: 1000, fill: 'forwards'});
+});
 
-})
+decrement.addEventListener('click', ()=>{
+    counter--;
+    counterValue.innerText = counter;
+    if(counterValue.innerText > 0) {
+        counterValue.style.color = '#3ddcef'; 
+    }
+    else if(counterValue.innerText == 0) {
+        counterValue.style.color = '#000';
+    } else {
+        counterValue.style.color = 'red';
+    }
+    counterValue.animate([
+        {
+            opacity: '0.5'
+        },
+        {
+            opacity: '1,0'
+        }
+    ], {duration: 1000, fill: 'forwards'});
+});
+
+reset.addEventListener('click', ()=>{
+    counter = 0;
+    counterValue.innerText = counter;
+    if(counterValue.innerText > 0) {
+        counterValue.style.color = '#3ddcef'; 
+    }
+    else if(counterValue.innerText == 0) {
+        counterValue.style.color = '#000';
+    } else {
+        counterValue.style.color = 'red';
+    }
+    counterValue.animate([
+        {
+            opacity: '0.5'
+        },
+        {
+            opacity: '1,0'
+        }
+    ], {duration: 1000, fill: 'forwards'});
+});
+
